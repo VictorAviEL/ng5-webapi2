90388aba4d1a29070ff3115dcbb2930e1908939f
Victor Annier Barrios Canizares <victor.barrios.canizares@gmail.com>
2018-02-25 16:54:33 -0500

    Cleaning unnecesary classes & Adding delai of 3 seconnds when saving an entity

---
+++ ng5/src/app/classes/entity.ts
@@ -0,0 +1,28 @@
+import { Stockholder } from './stockholder';
+import { Director } from './director';
+
+export class Entity {
+  id: number;
+  name: string;
+  form: string;
+  address: string;
+  dissolue: boolean;
+  date_creation: string;
+  date_fiscal: string;
+  activities: string;
+  stockholders: Array<Stockholder>;
+  directors: Array<Director>;
+
+
+   constructor(id:number){
+      this.id = id;
+      this.stockholders = [];
+      this.directors = [];
+   }
+
+
+
+
+
+
+}
---
+++ ng5/src/app/classes/stockholder.ts
@@ -0,0 +1,10 @@
+export class Stockholder {
+  id:number;
+  percent: number;
+  name: string;
+
+
+  constructor(id: number) {
+    this.id = id;
+}
+}
